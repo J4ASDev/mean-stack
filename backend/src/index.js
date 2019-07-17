dd32e6401a4a8505def345956a2aab5bdad8fb57
Jonathan Araujo <j4asdev@gmail.com>
2019-07-16 22:17:59 -0430

    Connected MongoDB with Mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+require('./db');
 const app = require('./app');
 const port = process.env.SERVER_PORT || 4000;
 
 const init = () => {
   app.listen(port);
-  console.log(`Server on port: ${port}`)
-};
+  console.log(`Server on port: ${port}`);
+}
 
 init();
